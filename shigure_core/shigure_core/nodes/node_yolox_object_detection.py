d318a9d9889d5ffc3ca9c02333bf411fb4d37777
aarimoto <67216471+arinnko5@users.noreply.github.com>
2022-12-14 20:32:23 +0900

    Update node_yolox_object_detection.py

--- shigure_core/shigure_core/nodes/node_yolox_object_detection.py
+++ shigure_core/shigure_core/nodes/node_yolox_object_detection.py
@@ -83,7 +83,7 @@ class YoloxObjectDetectionNode(ImagePreviewNode):
       timestamp = Timestamp(color_img_src.header.stamp.sec, color_img_src.header.stamp.nanosec)
       frame = ColorImageFrame(timestamp, self._color_img_buffer[0], color_img)
       self._color_img_frames.add(frame)
-      frame_object_dict = self.object_detection_logic.execute(subtraction_analysis_img, timestamp,
+      frame_object_dict = self.object_detection_logic.execute(yolox_bbox_src, timestamp,
                                                                 self.frame_object_list, self._judge_params)
       
       self.frame_object_list = list(chain.from_iterable(frame_object_dict.values()))
