ce9d8c4b86bb6866ed8f0a5e31e5b7851782c690
aarimoto <67216471+arinnko5@users.noreply.github.com>
2022-12-15 20:13:11 +0900

    Update node_yolox_object_detection.py

--- shigure_core/shigure_core/nodes/node_yolox_object_detection.py
+++ shigure_core/shigure_core/nodes/node_yolox_object_detection.py
@@ -90,9 +90,97 @@ class YoloxObjectDetectionNode(ImagePreviewNode):
       
       self.frame_object_list = list(chain.from_iterable(frame_object_dict.values()))
       
+      result_img = cv2.cvtColor(subtraction_analysis_img, cv2.COLOR_GRAY2BGR)
       
+      if self._color_img_frames.is_full():
+        self.get_logger().info('Buffering end', once=True)
+        
+        frame = self._color_img_frames.top_frame
+        
+        sec, nano_sec = frame.timestamp.timestamp
+        detected_object_list = DetectedObjectList()
+        detected_object_list.header.stamp.sec = sec
+        detected_object_list.header.stamp.nanosec = nano_sec
+        detected_object_list.header.frame_id = camera_info.header.frame_id
+        
+        timestamp_str = str(frame.timestamp)
+        if timestamp_str in frame_object_dict.keys():
+          frame_object_list = frame_object_dict.get(timestamp_str)
+          
+          # 検知開始時間が同じのフレームオブジェクトが検知済でなければ警告
+          if not all([frame_object.is_finished() for frame_object in frame_object_list]):
+            self.get_logger().warning('検知が終了していないオブジェクトを含んでいます')
+          
+          detected_object_list = self.create_msg(frame_object_list, detected_object_list, frame)
+          
+        self.detection_publisher.publish(detected_object_list)
+        
+        if self.is_debug_mode:
+          img = self.print_fps(result_img)
+          tile_img = cv2.hconcat([cv2.cvtColor(subtraction_analysis_img, cv2.COLOR_GRAY2BGR), img])
+          cv2.namedWindow('object_detection', cv2.WINDOW_NORMAL)
+          cv2.imshow("object_detection", tile_img)
+          cv2.waitKey(1)
+        else:
+          print(f'[{datetime.datetime.now()}] fps : {self.fps}', end='\r')
+        
+    def  create_msg(self, frame_object_list: List[FrameObject], detected_object_list: DetectedObjectList, frame: ColorImageFrame) -> DetectedObjectList:
+      for frame_object in frame_object_list:
+        action, bounding_box_src, size, mask_img, _ = frame_object.item.items
+        x, y, width, height = bounding_box_src.items
+        
+        detected_object = DetectedObject()
+        detected_object.action = action.value
+        detected_object.mask = self.bridge.cv2_to_compressed_imgmsg(mask_img, 'png')
+        
+        bounding_box = BoundingBox()
+        bounding_box.x = float(x)
+        bounding_box.y = float(y)
+        bounding_box.width = float(width)
+        bounding_box.height = float(height)
+        detected_object.bounding_box = bounding_box
+        
+        detected_object_list.object_list.append(detected_object)
+        
+        self.frame_object_list.remove(frame_object)
+        
+        if self.is_debug_mode:
+          item_color_img = frame.new_image if action == DetectedObjectActionEnum.BRING_IN else frame.old_image
+          print('オブジェクトが検出されました(',
+                f'action: {action.value}, x: {x}, y: {y}, width: {width}, height: {height}, size: {size})')
+          icon = np.zeros((height + 10, width, 3), dtype=np.uint8)
+          icon[0:height, 0:width, :] = item_color_img[y:y + height, x:x + width, :]
+          
+          img_height, img_width = item_color_img.shape[:2]
+          icon = cv2.resize(icon.copy(), (img_width // 2, img_height // 2))
+          cv2.putText(icon, f'Action : {action.value}', (0, img_height // 2 - 5), cv2.FONT_HERSHEY_PLAIN, 1.5,
+                      (255, 255, 255), thickness=2)
+          
+          self.object_list[self.object_index] = icon
+          self.object_index = (self.object_index + 1) % 4
+          
+        return detected_object_list
+      
+def main(args=None):
+  rclpy.init(args=args)
+  
+  object_detection_node = ObjectDetectionNode()
+  
+  try:
+    rclpy.spin(object_detection_node)
     
+  except KeyboardInterrupt:
+    pass
+  
+  finally:
+    print()
+    # 終了処理
+    object_detection_node.destroy_node()
+    rclpy.shutdown()
+        
       
+if __name__ == '__main__':
+  main()
       
       
       
