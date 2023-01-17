b7c2b3fcb341a675d917b79ee0f3301905eca0a1
aarimoto <67216471+arinnko5@users.noreply.github.com>
2023-01-17 10:39:36 +0900

    Update Bbox_Object.py

--- shigure_core/shigure_core/nodes/yolox_object_detection/Bbox_Object.py
+++ shigure_core/shigure_core/nodes/yolox_object_detection/Bbox_Object.py
@@ -25,7 +25,7 @@ class BboxObject:
     	bbox_y = abs(self._bounding_box._y - other._bounding_box._y)
     	bbox_width = abs(self._bounding_box._width - other._bounding_box._width)
     	bbox_height = abs(self._bounding_box._height - other._bounding_box._height)
-        if self._class_id==other._class_id & bbox_x < 30 & bbox_y < 30 & bbox_width < 30 & bbox_height < 30:
+        if (self._class_id==other._class_id) & bbox_x < 30 & bbox_y < 30 & bbox_width < 30 & bbox_height < 30:
         	return True
         else if:
         	match_count += 1
