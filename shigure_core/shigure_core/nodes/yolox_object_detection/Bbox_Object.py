25a678e760d00d376040cf54b91ae4d91bfab3c4
aarimoto <67216471+arinnko5@users.noreply.github.com>
2023-01-18 16:00:34 +0900

    Update Bbox_Object.py

--- shigure_core/shigure_core/nodes/yolox_object_detection/Bbox_Object.py
+++ shigure_core/shigure_core/nodes/yolox_object_detection/Bbox_Object.py
@@ -12,42 +12,42 @@ class BboxObject:
     def __init__(self,  bounding_box:BoundingBox, size:int, mask_img:np.ndarray, started_at:Timestamp,class_id:str,found_count:int, not_found_count:int):
         self._bounding_box = bounding_box
         self._size = size
-        self._mask = mask
+        self._mask = mask_img
         self._started_at = started_at
         self._class_id = class_id
         self._found_count = 0
         self._not_found_count = 0
         
-        match_count = 0
+        self.not_match_count = 0
 
-    def is_match(self, other) -> Tuple[bool, int]:
+    def is_match(self, other):
     	bbox_x = abs(self._bounding_box._x - other._bounding_box._x)
     	bbox_y = abs(self._bounding_box._y - other._bounding_box._y)
     	bbox_width = abs(self._bounding_box._width - other._bounding_box._width)
     	bbox_height = abs(self._bounding_box._height - other._bounding_box._height)
-        if self._class_id==other._class_id & bbox_x < 30 & bbox_y < 30 & bbox_width < 30 & bbox_height < 30:
-        	return True
-        else:
-        	match_count += 1
+    	if (self._class_id==other._class_id)& bbox_x < 30 & bbox_y < 30: #& bbox_width < 30 & bbox_height < 30:
+    		self._found_count += 1
+    		self._started_at = other._started_at
+    		#print(self._found_count)
+    		return True
+    	else:
+        	other.not_match_count += 1
+        	self._not_found_count += 1
             
     def not_mach(self, List):
-    	if match_count >= len(List):
+    	if self.not_match_count > len(List):
     		return True
             
-    def add_found_count(self) -> None:
-    	self._found_count += 1
     
     def found_count_is(self):
-    	if self._found_count==10:
-    		
+    	if self._found_count==5:
+    		self._found_count = 0
+    		self._not_found_count = 0
     		return True
     		
-    def add_not_found_count(self):
-    	self._not_found_count += 1
     
     def not_found_count_is(self):
-    	if self._not_found_count == 10:
-    		
+    	if self._not_found_count >= 500:
     		return True
-
+    		
         
