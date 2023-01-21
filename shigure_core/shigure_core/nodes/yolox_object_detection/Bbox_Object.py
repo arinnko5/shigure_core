2997aca70ef03226deb477dc5791dbac86febc62
aarimoto <67216471+arinnko5@users.noreply.github.com>
2023-01-21 12:03:33 +0900

    Update Bbox_Object.py

--- shigure_core/shigure_core/nodes/yolox_object_detection/Bbox_Object.py
+++ shigure_core/shigure_core/nodes/yolox_object_detection/Bbox_Object.py
@@ -9,45 +9,28 @@ from shigure_core.nodes.common_model.bounding_box import BoundingBox
 
 class BboxObject:
 
-    def __init__(self,  bounding_box:BoundingBox, size:int, mask_img:np.ndarray, started_at:Timestamp,class_id:str,found_count:int, not_found_count:int):
+    def __init__(self,  bounding_box:BoundingBox, size:int, mask_img:np.ndarray, found_at:Timestamp,class_id:str):
         self._bounding_box = bounding_box
         self._size = size
         self._mask = mask_img
-        self._started_at = started_at
+        self._found_at = found_at
         self._class_id = class_id
-        self._found_count = 0
-        self._not_found_count = 0
+        self.fhist = [] # 各フレームでそのbboxが見つかったかどうかの履歴(True or False)
+        self.is_exist_start = False
+        self.is_exist_wait = False
+        self.is_exist_bring = False
         
-        self.not_match_count = 0
 
     def is_match(self, other):
     	bbox_x = abs(self._bounding_box._x - other._bounding_box._x)
     	bbox_y = abs(self._bounding_box._y - other._bounding_box._y)
     	bbox_width = abs(self._bounding_box._width - other._bounding_box._width)
     	bbox_height = abs(self._bounding_box._height - other._bounding_box._height)
-    	if (self._class_id==other._class_id)& bbox_x < 30 & bbox_y < 30: #& bbox_width < 30 & bbox_height < 30:
-    		self._found_count += 1
-    		self._started_at = other._started_at
+    	if (self._class_id==other._class_id)and(bbox_x < 100) and (bbox_y < 100)and(bbox_width < 100): #& bbox_width < 30 & bbox_height < 30:
+    		
     		return True
     	else:
-    		other.not_match_count += 1
-    		self._not_found_count += 1
+    		
     		return False
     		
-    	
-    def not_mach(self, List):
-    	if self.not_match_count > len(List):
-    		return True
-            
     
-    def found_count_is(self):
-    	if self._found_count==5:
-    		self._found_count = 0
-    		self._not_found_count = 0
-    		return True
-    		
-    
-    def not_found_count_is(self):
-    	if self._not_found_count >= 500:
-    		return True
-    		
